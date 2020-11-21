2f6ff2f6788479ebca260e2cdb01cbf7dadb8bc7
Brandon Kwong <brandon@brandonkwong.com>
2020-11-21 00:09:23 -0800

    Setup initial lib/haiku.js to getLines()

--- scripts/invoke.js
+++ scripts/invoke.js
@@ -1,9 +1,12 @@
 const { getVerse } = require('../lib/verse');
+const { generateHaiku } = require('../lib/haiku');
 
 async function invoke (options = {}) {
   try {
     const verse = await getVerse(options);
-    console.log(verse);
+    const haiku = generateHaiku(verse);
+
+    console.log(haiku);
   } catch (error) {
     console.error(error);
   }
